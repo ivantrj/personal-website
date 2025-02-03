d43630d5a379dc8066d886bea25c3ccaec937de3
Ivan Trajanovski <ivantrajanovski@gmail.com>
2025-02-03 20:20:22 +0100

    add featured projcets

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,7 @@
 
 export default new Map([
-["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")],
-["src/content/blog/indie-app-development.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Findie-app-development.mdx&astroContentModuleFlag=true")],
-["src/content/blog/developer-growth.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeveloper-growth.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/ai-in-mobile-apps.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-in-mobile-apps.mdx&astroContentModuleFlag=true")],
-["src/content/blog/weekly-reflection.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fweekly-reflection.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/developer-growth.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdeveloper-growth.mdx&astroContentModuleFlag=true")],
+["src/content/blog/weekly-reflection.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fweekly-reflection.mdx&astroContentModuleFlag=true")],
+["src/content/blog/indie-app-development.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Findie-app-development.mdx&astroContentModuleFlag=true")]]);
 		
