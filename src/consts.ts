08231e23c89a620f9d5e9a2aaab6958e03dc1f93
Ivan Trajanovski <ivantrajanovski@gmail.com>
2025-02-09 11:28:32 +0100

    COMPLETE REDEISGN

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Ivan Trajanovski'
-export const SITE_DESCRIPTION = 'Mobile Developer based in Austria | Building amazing mobile experiences'
---
+++ src/lib/constants.ts
@@ -0,0 +1,106 @@
+import type { Site, Page } from './types'
+
+export const loaderAnimation = [
+  '.loader',
+  { opacity: [1, 0], pointerEvents: 'none' },
+  { easing: 'ease-out' },
+]
+
+export const LINKS = {
+  github: 'https://github.com/ivantrj',
+  linkedin: 'https://www.linkedin.com/in/ivantrj/',
+  mail: 'mailto:hello.ivantrj@gmail.com',
+  instagram: 'https://www.instagram.com/ivantrjdev/',
+  twitter: 'https://www.x.com/ivan_trj',
+  // medium: 'https://ladvace.medium.com/',
+  // discord: 'https://discordapp.com/users/163300027618295808',
+}
+
+// Global
+export const SITE: Site = {
+  TITLE: 'Astro Sphere',
+  DESCRIPTION:
+    'Welcome to Astro Sphere, a portfolio and blog for designers and developers.',
+  AUTHOR: 'Mark Horn',
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: 'Work',
+  DESCRIPTION: 'Places I have worked.',
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: 'Blog',
+  DESCRIPTION: 'Writing on topics I am passionate about.',
+}
+
+// Projects Page
+export const PROJECTS: Page = {
+  TITLE: 'Projects',
+  DESCRIPTION: 'Recent projects I have worked on.',
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: 'Search',
+  DESCRIPTION: 'Search all posts and projects by keyword.',
+}
+
+// Study Page
+export const STUDIES = [
+  {
+    title: 'Computer Science',
+    institution: 'Universidad Central',
+    link: 'https://www.ucentral.edu.co/',
+    date: '2019 - 2027',
+  },
+  {
+    title: '...',
+    institution: 'Platzi',
+    link: 'https://platzi.com/',
+    date: '2018 - 2022',
+  },
+  {
+    title: '...',
+    institution: 'Udemy',
+    link: 'https://www.udemy.com/',
+    date: '2018 - 2022',
+  },
+  {
+    title: '...',
+    institution: 'Youtube',
+    link: 'https://www.youtube.com/@ivantrj',
+    date: '2018 - 2022',
+  },
+]
+
+export const EXPERIENCE = [
+  {
+    company: 'ruwido',
+    location: 'Salzburg, Austria',
+    position: 'iOS & Android Developer',
+    start: '2022',
+    link: 'https://ruwido.com/',
+    end: 'Current',
+    tasks: [
+      'Developing and maintaining current Flutter apps',
+      // 'Develo',
+    ],
+  },
+  {
+    company: 'E-Group',
+    location: 'Budapest, Hungary',
+    position: 'Software Engineer',
+    link: 'https://egroup.hu/',
+    start: '2019',
+    end: 'Current',
+    tasks: [
+      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
+      'Development map with the use of Google Maps API',
+      'Development internal platform with the use of React, Redux, and Material UI',
+      'Testing and debugging',
+    ],
+  },
+]
